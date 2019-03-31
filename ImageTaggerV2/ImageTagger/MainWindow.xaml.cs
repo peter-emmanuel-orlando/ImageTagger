854acba86d1fe13d05083f8cc44fe73b03123537
Peter Orlando <peter.emmanuel.orlando@gmail.com>
2019-03-31 07:16:48 -0400

    dynamically pulls up  images so as to not over use all the ram. currently goes back to the top whenever it loads a new batch. currently doesnt load more when window resizes. to solve this just load a proxy for all images then dynamically fill them out and pop in/out images

--- ImageTaggerV2/ImageTagger/MainWindow.xaml.cs
+++ ImageTaggerV2/ImageTagger/MainWindow.xaml.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -26,12 +29,14 @@ namespace ImageTagger
 
 
         private string programDirectory = "";
-        private string sourceDirectory = "";
         private string destinationDirectory = "";
         private readonly string TagsDisplayPlaceholder = "[type tags for image]";
         private bool checkForEmptyTags = true;
         private bool randomizeImages = false;
+
+        private string sourceDirectory = @"C:\Users\YumeMura\Downloads";
         private readonly List<string> imageFileNames = new List<string>();
+        private readonly List<ImageSquare> loadedImages = new List<ImageSquare>();
 
         private const string locFileName = @"lastSession.loc";
         private const string genericLocFile = @"SourceDirectory:SourcePlaceHolder|DestinationDirectory:DestinationPlaceHolder";
@@ -40,16 +45,18 @@ namespace ImageTagger
         public MainWindow()
         { 
             InitializeComponent();
-            var tmp = GetImageFilenames(@"C:\Users\YumeMura\Downloads");
-            var imageSquares = new List<ImageSquare>();
+            imageFileNames.Clear();
+            imageFileNames = GetImageFilenames(sourceDirectory, true);
+            loadedImages.Clear();
+            loadedImages = new List<ImageSquare>();
             int count = 0;
             int max = 25;
-            foreach (var filename in tmp)
+            foreach (var filename in imageFileNames)
             {
                 Debug.WriteLine(filename);
                 try
                 {
-                    imageSquares.Add(new ImageSquare(filename));
+                    loadedImages.Add(new ImageSquare(filename));
                     count++;
                 }
                 catch (System.NotSupportedException)
@@ -58,51 +65,27 @@ namespace ImageTagger
                 }
                 if (count >= max) break;
             }
-            imgGrid.ItemsSource = imageSquares;
+            imgGrid.ItemsSource = loadedImages.ToArray();
+            if(count > 0)
+                imgGrid.SelectedIndex = 0;
         }
 
         /*
-        private void Initialize()
+        public enum FileSortMethod
         {
+            Name = 0,
+            Date,
+            Random
+        }
 
-
-            programDirectory = Directory.GetCurrentDirectory();
-            try
-            {
-                // Read the file and display it line by line.
-                var filename = programDirectory + @"\" + locFileName;
-                System.IO.FileStream fs = new FileStream(filename, FileMode.OpenOrCreate);
-                System.IO.StreamReader file = new System.IO.StreamReader(fs);
-                var lines = file.ReadToEnd().Split('|');
-                file.Close();
-                fs.Close();
-
-                sourceDirectory = lines[0].Replace("SourceDirectory:", "");
-                destinationDirectory = lines[1].Replace("DestinationDirectory:", "");
-
-                if (!Directory.Exists(sourceDirectory))
-                    sourceDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
-                if (!Directory.Exists(destinationDirectory))
-                    destinationDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
-
-                LoadSourceDirectory(sourceDirectory);
-            }
-            catch (Exception)
-            {
-
-                LoadSourceDirectory(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures));
-                destinationDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
-            }
-
-            //CheckForEmptyTagsToolStripMenuItem.Text = "Check For Empty Tags: " + ((checkForEmptyTags) ? "Yes" : "No");
-            //CheckForEmptyTagsToolStripMenuItem.BackColor = (checkForEmptyTags) ? SystemColors.MenuHighlightColor : SystemColors.ControlColor;
-
-            //RandomizeImagesNoToolStripMenuItem.Text = "Randomize Images: " + ((randomizeImages) ? "Yes" : "No");
-            //RandomizeImagesNoToolStripMenuItem.BackColor = (randomizeImages) ? SystemColors.MenuHighlightColor : SystemColors.ControlColor;
+        public enum FileSortDirection
+        {
+            Ascending = 0,
+            Descending
         }
         */
 
-        private List<string> GetImageFilenames(string sourcePath)
+        private List<string> GetImageFilenames(string sourcePath, bool randomize = false)
         {
             var result = new List<string>();
             var fileTypes = new string[] { ".jpg" };//, "jpeg", "gif", "png", };
@@ -111,14 +94,56 @@ namespace ImageTagger
                 if (fileTypes.Contains( Path.GetExtension(fileName)))
                     result.Add(fileName);
             }
+            if (randomize) result.Shuffle();
             return result;
         }
 
-        private void SetAsMainImage(object sender, SelectionChangedEventArgs e)
+        private void HandleImageGridSelectionChange(object sender, SelectionChangedEventArgs e)
         {
-            //var img = sender as Image;
-            Debug.WriteLine("selected: " + (e.AddedItems[0] as ImageSquare).ImgSource.UriSource);
-            mainImageDisplay.Source = (e.AddedItems[0] as ImageSquare).ImgSource;
+            if(e.AddedItems.Count > 0)
+            {
+                Debug.WriteLine("selected: " + (e.AddedItems[0] as ImageSquare).ImgSource.UriSource);
+                mainImageDisplay.Source = (e.AddedItems[0] as ImageSquare).ImgSource;
+            }
+        }
+
+        private void HandleImageGridScrollChange(object sender, ScrollChangedEventArgs e)
+        {
+            Debug.WriteLine(
+                " e.VerticalChange:" +
+                e.VerticalChange + 
+                "e.ViewportHeight:" +
+                e.ViewportHeight +
+                " e.VerticalOffset:" +
+                e.VerticalOffset +
+                " e.ExtentHeight:" +
+                e.ExtentHeight +
+                " e.ExtentHeightChange:" +
+                e.ExtentHeightChange
+              );
+            if (e.VerticalChange > 0 || e.ExtentHeightChange != 0)
+            {
+                if (e.VerticalOffset + e.ViewportHeight == e.ExtentHeight)
+                {
+                    Debug.WriteLine("At the bottom of the list!");
+                    if(loadedImages.Count() < imageFileNames.Count())
+                    {
+                        var loadedCount = loadedImages.Count();
+                        var fileNameCount = imageFileNames.Count();
+                        for (int i = loadedCount; i < Math.Min(10 + loadedCount, fileNameCount ); i++)
+                        {
+                            try
+                            {
+                                //Debug.WriteLine("trying to add file to list:" + imageFileNames[i]);
+                                var newSquare = new ImageSquare(imageFileNames[i]);
+                                loadedImages.Add(newSquare);
+                            }
+                            catch (Exception) { }
+                        }
+                        imgGrid.ItemsSource = loadedImages.ToArray();
+                    }
+                }
+            }
         }
     }
 }
---
+++ ImageTaggerV2/ImageTagger/ThreadSafeRandom.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class ThreadSafeRandom
+{
+    [ThreadStatic] private static System.Random Local;
+
+    private static System.Random ThisThreadsRandom
+    {
+        get { return Local ?? (Local = new System.Random(unchecked(System.Environment.TickCount * 31 + System.Threading.Thread.CurrentThread.ManagedThreadId))); }
+    }
+
+    public static void Shuffle<T>(this IList<T> list)
+    {
+        int n = list.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = ThreadSafeRandom.ThisThreadsRandom.Next(n + 1);
+            T value = list[k];
+            list[k] = list[n];
+            list[n] = value;
+        }
+    }
+    public static T Random<T>(this IList<T> list)
+    {
+        int k = ThreadSafeRandom.ThisThreadsRandom.Next(list.Count - 1);
+        return list[k];
+    }
+}
