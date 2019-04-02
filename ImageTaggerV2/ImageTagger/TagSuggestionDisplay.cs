a36280a845ad8c5a3c558c188e77bb0cc87e014e
Peter Orlando <peter.emmanuel.orlando@gmail.com>
2019-04-02 15:51:02 -0400

    added randomize suggested tags

--- ImageTaggerV2/ImageTagger/TagSuggestionDisplay.cs
+++ ImageTaggerV2/ImageTagger/TagSuggestionDisplay.cs
@@ -1,7 +1,9 @@
 ﻿using ImageTagger_DataModels;
 using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Timers;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace ImageTagger
@@ -11,35 +13,56 @@ namespace ImageTagger
     /// </summary>
     /// 
 
+
+    public class SuggestedTag
+    {
+        public string TagName { get; }
+        public int OffsetX { get; }
+        public int OffsetY { get; }
+
+        public SuggestedTag(string tagName, int offsetX, int offsetY)
+        {
+            TagName = tagName;
+            OffsetX = offsetX;
+            OffsetY = offsetY;
+        }
+    }
+
     public class TagSuggestionDisplay
     {
         MainWindow main { get; }
         ListBox TagSuggestion { get { return main.tagSuggestionDisplay; } }
 
-        private ObservableCollection<ImageTag> SuggestedTags { get; } = new ObservableCollection<ImageTag>();
-
-        private Timer ChangeSuggestions;
-
+        private ObservableCollection<SuggestedTag> SuggestedTags { get; } = new ObservableCollection<SuggestedTag>();
+        
         public TagSuggestionDisplay(MainWindow main)
         {
             this.main = main;
             TagSuggestion.ItemsSource = SuggestedTags;
             main.PreviewMainWindowUnload += UnsubscribeFromAllEvents;
+
+            main.reloadTagSuggestions.Click += HandleChangeSuggestionsClickEvent;
+            ChangeSuggestions();
         }
 
-        private void SetTimer()
+        private void HandleChangeSuggestionsClickEvent(object sender, RoutedEventArgs e)
         {
-            // Create a timer with a two second interval.
-            ChangeSuggestions = new Timer(4000);
-            // Hook up the Elapsed event for the timer. 
-            ChangeSuggestions.Elapsed += OnTimedEvent;
-            ChangeSuggestions.AutoReset = true;
-            ChangeSuggestions.Enabled = true;
+            ChangeSuggestions();
         }
 
-        private void OnTimedEvent(Object source, ElapsedEventArgs e)
+        private void ChangeSuggestions()
         {
-            //SuggestedTags.Add(//( DirectoryTagUtil.GetTags("loaded").GetEnumerator());
+            var maxOffsetX = TagSuggestion.ActualWidth - 70;
+            var maxOffsetY = TagSuggestion.ActualHeight - 70;
+
+            SuggestedTags.Clear();
+            var list = new List<string>(DirectoryTagUtil.GetTags("loaded"));
+            list.Shuffle();
+            var r =  new Random(DateTime.UtcNow.Millisecond);
+            foreach ( string tagName in list)
+            {
+                SuggestedTags.Add(new SuggestedTag(tagName, (int)(r.Next() * maxOffsetX), (int)(r.Next() * maxOffsetY)));
+            }
         }
 
         private void UnsubscribeFromAllEvents(object sender, EventArgs e)
