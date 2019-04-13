b388e630b8d50cfd331265173d2c5eeac8377bca
Peter Orlando <peter.emmanuel.orlando@gmail.com>
2019-04-13 17:37:12 -0400

    changed main window to one called Launcher

---
+++ ImageTaggerV2/ImageTagger/Launcher.xaml.cs
@@ -0,0 +1,18 @@
+﻿
+using Microsoft.WindowsAPICodePack.Shell;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Windows;
+
+namespace ImageTagger
+{
+    public partial class Launcher : Window
+    {
+        public Launcher()
+        {
+            InitializeComponent();
+        }
+    }
+}
