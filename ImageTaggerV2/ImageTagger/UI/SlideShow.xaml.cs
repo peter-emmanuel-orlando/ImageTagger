d48ccf734cc9cf4bea50344ac64b84b55b0e8159
Peter Orlando <peter.emmanuel.orlando@gmail.com>
2019-05-14 14:14:54 -0400

    added beginning of slideshow

---
+++ ImageTaggerV2/ImageTagger/UI/SlideShow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ImageTagger.UI
+{
+    /// <summary>
+    /// Interaction logic for SlideShow.xaml
+    /// </summary>
+    public partial class SlideShow : Window
+    {
+        public SlideShow()
+        {
+            InitializeComponent();
+        }
+    }
+}
