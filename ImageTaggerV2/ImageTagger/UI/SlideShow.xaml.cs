adae6328bd569bcdd8c52065371a1a0a5a02b7e3
Peter Orlando <peter.emmanuel.orlando@gmail.com>
2019-05-15 00:02:57 -0400

    added features to slide show

--- ImageTaggerV2/ImageTagger/UI/SlideShow.xaml.cs
+++ ImageTaggerV2/ImageTagger/UI/SlideShow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using ImageTagger.DataModels;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -25,6 +26,11 @@ namespace ImageTagger.UI
         private ImageFiles ImageFiles { get; }
         private ImageInfo mainImageInfo { get; } = new ImageInfo();
         private int currentIndex { get; set; } = 0;
+
+
+        private bool isPlaying = true;
+        private bool resetTimer = false;
+        private int imgDelay = 2000;
         public static int PlaySlideshow(ImageFiles imageFiles, int startIndex = 0)
         {
             var slideShow = new SlideShow(imageFiles, startIndex);
@@ -35,8 +41,9 @@ namespace ImageTagger.UI
         {
             InitializeComponent();
             ImageFiles = imageFiles;
-            currentIndex = startIndex - 1;
+            currentIndex = startIndex - 2;//because textchanged will be called
             mainSlideshowImageDisplay.DataContext = mainImageInfo;
+            slideshowSpeed.Text = imgDelay + "";
             Play();
         }
 
@@ -44,25 +51,83 @@ namespace ImageTagger.UI
         {
             Task.Run(new Action(async () =>
             {
-                while(true)
+                while(isPlaying)
                 {
                     await App.Current.Dispatcher.InvokeAsync(new Action(() =>
                     {
-                        NextImage();
+                        ChangeImageIndex();
                     }));
-                    Thread.Sleep(2000);
+                    resetTimer = true;
+                    while (resetTimer)
+                    {
+                        resetTimer = false;
+                        Thread.Sleep(imgDelay);
+                    }
                 }
             }));
         }
 
-        private void NextImage()
+        private void Stop()
+        {
+            isPlaying = false;
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            Stop();
+        }
+
+        private void ChangeImageIndex(int deltaIndex = 1)
         {
             if(ImageFiles.Count > 0)
             {
-                currentIndex = (currentIndex +1) % ImageFiles.Count;
+                currentIndex = (currentIndex + deltaIndex) % ImageFiles.Count;
+                while (currentIndex < 0)
+                {
+                    currentIndex = ImageFiles.Count + currentIndex;
+                }
                 mainImageInfo.ImgPath = ImageFiles.Get(currentIndex);
                 mainImageInfo.Load(-1, DispatcherPriority.Send);
                 //ImageDisplay.Source = mainImageInfo;
+            }
+        }
+
+        private void HandleMouseOverMenuPanel(object sender, MouseEventArgs e)
+        {
+            menu.Visibility = Visibility.Visible;
+        }
+
+        private void HandleMouseOffMenuPanel(object sender, MouseEventArgs e)
+        {
+            menu.Visibility = Visibility.Hidden;
+        }
+
+        private void SlideshowSpeed_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (float.TryParse(slideshowSpeed.Text, out float newDelay))
+            {
+                imgDelay = (int)newDelay;
+                if (imgDelay < 500)
+                    imgDelay = 500;
+                ChangeImageIndex();
+            }
+            slideshowSpeed.Text = imgDelay + "";
+        }
+
+        private void HandleKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Right)
+            {
+                ChangeImageIndex();
+                e.Handled = true;
+                resetTimer = true;
+            }
+            else if (e.Key == Key.Left)
+            {
+                ChangeImageIndex(-1);
+                e.Handled = true;
+                resetTimer = true;
             }
         }
     }
